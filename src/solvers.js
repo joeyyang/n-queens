d4c4070090fdace496b16665c4525d4c5935156f
Hack Reactor <hackreactor@Hacks-Mac-mini-2.local>
2013-10-02 22:01:11 -0700

    completes countNQueensSolutions()

--- src/solvers.js
+++ src/solvers.js
@@ -119,7 +119,52 @@ window.findNQueensSolution = function(n){
 
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
 window.countNQueensSolutions = function(n){
-  var solutionCount = undefined; //fixme
+  if (n === 0) return 1;
+  var solutionCount = 0; 
+
+  //initialize n x n board
+  var board = new Board({n:n});
+
+  // backtracking algorithm with no use of symmetry or other further optimization
+
+  // for every row, run recursive routine:
+  //   place a queen in 0th column of the row.
+  //   if no queen conflict:
+  //     if in row n-1 (last row), increment solnCounter
+  //     else move to next row. (recur)
+  //   if queen conflict:
+  //     if not in last column:
+  //       place queen in next column
+  //     else:
+  //       return queen conflict indicator, continue algorithm at previous row
+
+  //   when we try to move the queen in the 0th row to column n, we return solnCounter.  
+
+
+  var queensBacktrack = function(row){
+    for(var col = 0; col < n; col++){
+      var currentBoard = JSON.stringify(board.rows());
+      // if (col > 0) board.rows()[row][col-1] = 0;
+      board.rows()[row][col] = 1;
+      if(! board.hasAnyQueensConflicts()){
+        if (row === (n-1)){
+          solutionCount++;
+          board.rows()[row][col] = 0;
+          if (col === (n-1)) return;
+        }
+        else {
+          queensBacktrack(row+1);  // set return value
+          board.rows()[row][col] = 0;
+          if (col === (n-1)) return;
+        }
+      } else{
+        board.rows()[row][col] = 0;
+        if(col === (n-1)) return;
+      }   
+    }
+  }; 
+
+  queensBacktrack(0);
 
   console.log('Number of solutions for ' + n + ' queens:', solutionCount);
   return solutionCount;
